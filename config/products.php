531cd6fdc20759c23860f7d853c09fb3242dcecd
Kumara <axellealvaro67@gmail.com>
2025-01-01 23:52:30 +0700

    main

--- config/products.php
+++ config/products.php
@@ -3,18 +3,90 @@
 return array (
   0 => 
   array (
-    'id' => 1735448041,
-    'name' => 'Ayam fff',
-    'price' => '500004444',
-    'description' => 'Ayam Arab',
-    'image' => 'https://www.joelsartore.com/wp-content/uploads/stock/ANI054/ANI054-00052-1920x1279.jpg',
+    'id' => 1735749393,
+    'name' => 'Iphone 12',
+    'price' => '7499000',
+    'description' => '-	Layar Super Retina XDR
+-	Layar OLED sepenuhnya 6,1 inci (diagonal)
+-	Resolusi 2532 x 1170 piksel pada 460 ppi
+-	Layar HDR
+-	True Tone
+-	Warna luas (P3)
+-	Haptic Touch
+-	Rasio kontras 2.000.000:1 (umum)
+-	Kecerahan maks 625 nit (umum); kecerahan puncak 1.200 nit (HDR)
+-	Lapisan oleophobic anti sidik jari
+-	Mendukung tampilan berbagai bahasa dan karakter secara bersamaan',
+    'image' => 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-pro-family-hero?wid=940&hei=1112&fmt=png-alpha&.v=1604021660000',
+  ),
+  1 => 
+  array (
+    'id' => 1735749507,
+    'name' => 'Iphone 11',
+    'price' => '6000000',
+    'description' => '-	Layar Liquid Retina HD
+-	Layar LCD Multi-Touch sepenuhnya 6,1 inci (diagonal) dengan teknologi IPS
+-	Resolusi 1792 x 828 piksel pada 326 ppi
+-	Rasio kontras 1400:1 (umum)
+-	Layar True Tone
+-	Layar warna luas (P3)
+-	Haptic Touch
+-	Kecerahan maks 625 nit (umum)
+-	Lapisan oleophobic anti sidik jari
+-	Mendukung tampilan berbagai bahasa dan karakter secara bersamaan',
+    'image' => 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-11-pro-max-gold-select-2019?wid=940&hei=1112&fmt=png-alpha&.v=1566953859459',
   ),
   2 => 
   array (
-    'id' => 1735470567,
-    'name' => 'anjing',
+    'id' => 1735749573,
+    'name' => 'Iphone X',
+    'price' => '3000000',
+    'description' => '-	Layar Super Retina HD
+-	Layar OLED Multi-Touch menyeluruh 5,8 inci (diagonal)
+-	Layar HDR
+-	Resolusi 2436 x 1125 piksel pada 458 ppi
+-	Rasio kontras 1.000.000:1 (umum)
+-	Layar True Tone
+-	Layar warna luas (P3)
+-	3D Touch
+-	Kecerahan maks 625 cd/m² (umum)
+-	Lapisan oleophobic anti sidik jari
+-	Mendukung tampilan berbagai bahasa dan karakter secara bersamaan',
+    'image' => 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-xr-blue-select-201809?wid=940&hei=1112&fmt=png-alpha&.v=1551226038669',
+  ),
+  3 => 
+  array (
+    'id' => 1735749633,
+    'name' => 'Iphone 8',
+    'price' => '1500000',
+    'description' => '-	Layar True Tone
+-	Layar warna luas (P3)
+-	Haptic Touch
+-	Kecerahan maks 625 nit (umum)
+-	Piksel domain-ganda untuk sudut pandang yang luas
+-	Lapisan oleophobic anti sidik jari
+-	Mendukung tampilan berbagai bahasa dan karakter secara bersamaan
+-	Pembesar Tampilan
+-	Reachability (Keterjangkauan)',
+    'image' => 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone8-plus-silver-select-2018?wid=940&hei=1112&fmt=png-alpha&.v=1551226038669',
+  ),
+  4 => 
+  array (
+    'id' => 1735749700,
+    'name' => 'Iphone 7',
     'price' => '1000000',
-    'description' => 'anjing lu',
-    'image' => 'https://cdn0-production-images-kly.akamaized.net/eZpobSxVWcvWa-ohwoE-0-VCM4U=/500x500/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4572116/original/092219100_1694498707-1.jpg',
+    'description' => '-	Layar Retina HD
+-	Layar lebar LCD
+-	Multi-Touch 4,7 inci (diagonal) dengan teknologi IPS
+-	Resolusi 1334 x 750 piksel pada 326 ppi
+-	Rasio kontras 1400:1 (umum)
+-	Layar warna luas (P3)
+-	Kecerahan maks 625 cd/m2 (umum)
+-	Piksel domain-ganda untuk sudut pandang yang luas
+-	Lapisan oleophobic anti sidik jari
+-	Mendukung tampilan berbagai bahasa dan karakter secara bersamaan
+-	Pembesar Tampilan
+-	Reachability (Keterjangkauan)',
+    'image' => 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone7-black-select-2016?wid=940&hei=1112&fmt=png-alpha&v=1551226038669',
   ),
 );
--- resources/views/layouts/adminapp.blade.php
+++ resources/views/layouts/adminapp.blade.php
@@ -35,7 +35,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin')}}">
                 <div class="sidebar-brand-text mx-3">Dashboard Admin</sup></div>
             </a>
 
@@ -90,31 +90,9 @@
 
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
                             </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
                         </li>
 
                     </ul>
@@ -133,16 +111,6 @@
     </script>
             </div>
             <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
 
         </div>
         <!-- End of Content Wrapper -->
